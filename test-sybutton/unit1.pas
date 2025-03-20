20544854cd11ef9aab980d255c364dc72a1c9e19
Andry Smirnov <Andry.Smirnov@gmail.com>
2025-03-20 13:55:03 +0300

    Добавит тестовый проект для TSYButton

---
+++ test-sybutton/unit1.pas
@@ -0,0 +1,32 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, sypanel, sybutton,
+  emisgrid;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    EmiStringGrid1: TEmiStringGrid;
+    SYButton1: TSYButton;
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
