2e3a7f91ab21778e8678572b87839b67fd91bfb8
Andry Smirnov <Andry.Smirnov@gmail.com>
2025-04-13 22:58:22 +0300

    Добавит скрипты lazarus-install-packages/install

---
+++ lazarus-install-packages/install.cmd
@@ -0,0 +1,6 @@
+lazbuild .\lib\MiniCommons.lpk
+lazbuild .\xml\source\MiniXML.lpk
+lazbuild .\socket\source\MiniSockets.lpk
+lazbuild .\connection\src\MiniConnections.lpk
+lazbuild .\comm\source\MiniComm.lpk
+pause
