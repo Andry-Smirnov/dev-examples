f04d92381a187d6fac53211ca1726ce7ed5dfab5
Andry Smirnov <Andry.Smirnov@gmail.com>
2025-04-21 14:15:51 +0300

    Переименует модуль test-sybutton/unit1.pas в test-sybutton/main.pas

---
+++ test-sybutton/main.pas
@@ -0,0 +1,142 @@
+unit main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
+  sybutton, sypanel
+  ;
+
+type
+
+  { TMainForm }
+
+  TMainForm = class(TForm)
+    Bevel1: TBevel;
+    Bevel2: TBevel;
+    Bevel3: TBevel;
+    Button1: TButton;
+    Label1: TLabel;
+    Label10: TLabel;
+    Label11: TLabel;
+    Label12: TLabel;
+    Label13: TLabel;
+    Label14: TLabel;
+    Label15: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    Label7: TLabel;
+    Label8: TLabel;
+    Label9: TLabel;
+    SYButton1: TSYButton;
+    SYButton10: TSYButton;
+    SYButton11: TSYButton;
+    SYButton12: TSYButton;
+    SYButton13: TSYButton;
+    SYButton14: TSYButton;
+    SYButton15: TSYButton;
+    SYButton16: TSYButton;
+    SYButton17: TSYButton;
+    SYButton18: TSYButton;
+    SYButton19: TSYButton;
+    SYButton2: TSYButton;
+    SYButton20: TSYButton;
+    SYButton21: TSYButton;
+    SYButton22: TSYButton;
+    SYButton23: TSYButton;
+    SYButton24: TSYButton;
+    SYButton25: TSYButton;
+    SYButton26: TSYButton;
+    SYButton27: TSYButton;
+    SYButton28: TSYButton;
+    SYButton29: TSYButton;
+    SYButton3: TSYButton;
+    SYButton30: TSYButton;
+    SYButton31: TSYButton;
+    SYButton32: TSYButton;
+    SYButton33: TSYButton;
+    SYButton34: TSYButton;
+    SYButton35: TSYButton;
+    SYButton36: TSYButton;
+    SYButton37: TSYButton;
+    SYButton38: TSYButton;
+    SYButton39: TSYButton;
+    SYButton4: TSYButton;
+    SYButton40: TSYButton;
+    SYButton41: TSYButton;
+    SYButton42: TSYButton;
+    SYButton43: TSYButton;
+    SYButton44: TSYButton;
+    SYButton45: TSYButton;
+    SYButton46: TSYButton;
+    SYButton47: TSYButton;
+    SYButton48: TSYButton;
+    SYButton49: TSYButton;
+    SYButton5: TSYButton;
+    SYButton50: TSYButton;
+    SYButton51: TSYButton;
+    SYButton52: TSYButton;
+    SYButton53: TSYButton;
+    SYButton54: TSYButton;
+    SYButton55: TSYButton;
+    SYButton56: TSYButton;
+    SYButton57: TSYButton;
+    SYButton58: TSYButton;
+    SYButton59: TSYButton;
+    SYButton6: TSYButton;
+    SYButton60: TSYButton;
+    SYButton7: TSYButton;
+    SYButton8: TSYButton;
+    SYButton9: TSYButton;
+    SYPanel1: TSYPanel;
+    SYPanel10: TSYPanel;
+    SYPanel11: TSYPanel;
+    SYPanel12: TSYPanel;
+    SYPanel13: TSYPanel;
+    SYPanel14: TSYPanel;
+    SYPanel15: TSYPanel;
+    SYPanel16: TSYPanel;
+    SYPanel17: TSYPanel;
+    SYPanel18: TSYPanel;
+    SYPanel19: TSYPanel;
+    SYPanel2: TSYPanel;
+    SYPanel20: TSYPanel;
+    SYPanel21: TSYPanel;
+    SYPanel22: TSYPanel;
+    SYPanel23: TSYPanel;
+    SYPanel24: TSYPanel;
+    SYPanel3: TSYPanel;
+    SYPanel4: TSYPanel;
+    SYPanel5: TSYPanel;
+    SYPanel6: TSYPanel;
+    SYPanel7: TSYPanel;
+    SYPanel8: TSYPanel;
+    SYPanel9: TSYPanel;
+    procedure Button1Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  MainForm: TMainForm;
+
+implementation
+
+{$R *.lfm}
+
+{ TMainForm }
+
+procedure TMainForm.Button1Click(Sender: TObject);
+begin
+  Close;
+end;
+
+end.
+
